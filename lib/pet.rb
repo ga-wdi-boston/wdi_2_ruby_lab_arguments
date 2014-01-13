86f0f4692b9ca9a282c391d950e616011d928196
Mike Stone <mikestone14@gmail.com>
2014-01-13 15:23:53 -0500

    Refactored tests for pets

--- lib/pet.rb
+++ lib/pet.rb
@@ -1,10 +1,10 @@
 class Pet
 	attr_accessor :name, :species, :num_teeth, :num_claws, :num_backpacks
-	def initialize(name, options={num_teeth: 0, num_claws: 0, num_backpacks: 0})
+	def initialize(name, options={})
 		@name = name
 		@species = options[:species] || ['cat', 'dog', 'bunny wearing backpack'].sample
-		@num_teeth = options[:num_teeth]
-		@num_claws = options[:num_claws]
-		@num_backpacks = options[:num_backpacks]
+		@num_teeth = options[:num_teeth] || 0
+		@num_claws = options[:num_claws] || 4
+		@num_backpacks = options[:num_backpacks] || 0
 	end
 end
--- spec/pet_spec.rb
+++ spec/pet_spec.rb
@@ -13,13 +13,15 @@ describe Pet do
 			expect(['cat', 'dog', 'bunny wearing backpack']).to include valid_pet.species
 		end
 		it 'can have a number of teeth, claws, and backpacks' do
-			valid_pet = Pet.new("Chester", num_teeth: 2)
+			valid_pet = Pet.new("Chester", num_teeth: 2, num_backpacks: 3, num_claws: 2)
 			expect(valid_pet.num_teeth).to eq 2
+			expect(valid_pet.num_claws).to eq 2
+			expect(valid_pet.num_backpacks).to eq 3
 		end
-		it 'can have a number of teeth, claws, and backpacks' do
-			valid_pet = Pet.new("Chester", num_teeth: 2, num_backpacks: 0)
-			expect(valid_pet.num_teeth).to eq 2
-			expect(valid_pet.num_claws = 3).to eq 3
+		it 'set default values for teeth, claws, and backpacks' do
+			valid_pet = Pet.new("Chester")
+			expect(valid_pet.num_teeth).to eq 0
+			expect(valid_pet.num_claws).to eq 4
 			expect(valid_pet.num_backpacks).to eq 0
 		end
 	end
