435d2ce35f81406c468ee93a573f51efc24884cb
Janice Smith <janice.k.n.smith@gmail.com>
2014-01-13 16:29:45 -0500

    now person class works - first pass

---
+++ lib/person.rb
@@ -0,0 +1,34 @@
+# You should be able to create a new Person.
+# The person must be supplied a name. The person also has attributes of:
+# age (days)
+# weight (kg)
+# height (cm)
+# eye_color
+# hair_color
+# pets (array)
+
+# If no age is provided, then their age should be 0 days.
+
+# If no weight is provided, the weight should be randomly assigned between 60-100kg
+# If no height is provided, the height should be randomly assigned between 150-200cm
+# If no eye or hair colors are provided, don't do anything with them.
+# Write a method that returns their weight as stones).
+#All other properties should be able to be get/set.
+
+# Write a method that will allow them to greet any number
+#of other people objects (not strings) with a greeting (default value, "Hi").
+
+
+class Person
+	attr_accessor :name, :age,:number_of_pets, :eye_color, :hair_color, :weight, :height, :number_of_pets
+
+	def initialize(name, number_of_pets, age=0, options={})
+		@name = name
+		@age = age
+		@number_of_pets = [] << number_of_pets
+		@eye_color = options[:eye_color]
+		@hair_color = options[:hair_color]
+		@weight = options[:weight] || rand(60..100)
+		@height = options[:height] || rand(150..200)
+	end
+end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,2 +1,40 @@
 require 'spec_helper'
 require_relative "../lib/person"
+
+describe Person do
+	before(:each) do
+		@test_person_1 = Person.new("Jan", 1, 27, :eye_color => "brown", :hair_color => "black", :weight => 64 , :height => 158)
+	end
+
+	describe '#initialize' do
+		it 'initialize a new Person instance' do
+			expect(@test_person_1).to eq @test_person_1
+		end
+
+		it 'initialize a new person instance, check name' do
+			expect(@test_person_1.name).to eq "Jan"
+		end
+
+		it 'initialize a new person instance, check eye_color' do
+			expect(@test_person_1.eye_color).to eq "brown"
+		end
+
+		it 'initialize a new person instance, check hair_color' do
+			expect(@test_person_1.hair_color).to eq "black"
+		end
+
+		it 'initialize a new person instance, check weight' do
+			expect(@test_person_1.weight).to eq 64
+		end
+
+		it 'initialize a new person instance, check height' do
+			expect(@test_person_1.height).to eq 158
+		end
+
+		it 'initialize a new person instance, number_of_pets' do
+			expect(@test_person_1.number_of_pets).to eq [1]
+		end
+
+	end
+end
+
