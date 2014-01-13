46461ac3934c28664a26eaa9f8138a4546680641
Jordan Morano <jordan.morano@gmail.com>
2014-01-13 16:33:38 -0500

    Create describe #method shells for each method to build

--- spec/polygon_spec.rb
+++ spec/polygon_spec.rb
@@ -8,4 +8,29 @@ describe Polygon do
       expect(polygon.num_of_sides).to eq 3
     end
   end
+
+  describe '#sum_of_interior_angles' do
+    it '' do
+      expect()
+    end
+  end
+
+  describe '#number_of_corners' do
+    it '' do
+      expect()
+    end
+  end
+
+  describe '#degrees_of_freedom' do
+    it '' do
+      expect()
+    end
+  end
+
+  describe '#area' do
+    it '' do
+      expect()
+    end
+  end
+
 end
