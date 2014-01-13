621a385a44af52dfeafac6bd5a11439358288929
Mike Stone <mikestone14@gmail.com>
2014-01-13 16:44:41 -0500

    Added degrees_of_freedom with passing tests

--- lib/polygon.rb
+++ lib/polygon.rb
@@ -1,8 +1,11 @@
 class Polygon
 	attr_accessor :num_sides, :side_length_array
+	attr_reader :num_corners, :degrees_of_freedom
 	def initialize(*side_length)
 		@num_sides = side_length.length if side_length.length > 2
 		@side_length_array = side_length
+		@num_corners = @num_sides
+		@degrees_of_freedom = @num_sides * 2
 	end
 
 	def sum_of_interior_angles
--- spec/polygon_spec.rb
+++ spec/polygon_spec.rb
@@ -7,6 +7,14 @@ describe Polygon do
 			shape = Polygon.new(2,3,4)
 			expect(shape.num_sides).to eq 3
 		end
+		it 'should return the number of corners in the polygon' do
+			shape = Polygon.new(2,3,4)
+			expect(shape.num_corners).to eq 3
+		end
+		it 'should return the degrees of freedom in the polygon' do
+			shape = Polygon.new(2,3,4)
+			expect(shape.degrees_of_freedom).to eq 6
+		end
 	end
 
 	describe 'sum_of_interior_angles' do
@@ -15,4 +23,9 @@ describe Polygon do
 			expect(shape.sum_of_interior_angles).to eq 180
 		end
 	end
+
+	describe 'number_of_corners' do
+
+	end
+
 end
