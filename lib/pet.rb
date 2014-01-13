e5f4e461ac685c592e7b57a53b023c3f737c0404
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-01-13 16:10:01 -0500

    Pet done

--- lib/pet.rb
+++ lib/pet.rb
@@ -1,8 +1,15 @@
 class Pet
+	@@spec = ['dog', 'cat', 'bunny wearing backpack'][rand(3)]
 	attr_accessor :name, :species, :num_of_claw, :num_of_teeth, :num_of_backpack
-	def initialize(name, species = ["cat", "dog", "bunny wearing backpack"].random, num_of_claw, num_of_teeth, num_of_backpack)
+	
+	def random_species
+		species_option = ['dog', 'cat', 'bunny wearing backpack']
+		species = species_option[rand(3)]
+	end
+
+	def initialize(name, species = @@spec, num_of_claw, num_of_teeth, num_of_backpack)
 		@name = name
-		@species = gets.chomp
+		@species = species
 		@num_of_claw = num_of_claw
 		@num_of_teeth = num_of_teeth
 		@num_of_backpack = num_of_backpack
