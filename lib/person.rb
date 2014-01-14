6bb963a227df253012150d85d83f7143949fa6db
Alex Grant <alex@grantovich.net>
2014-01-14 11:44:33 -0500

    Add age, weight, height, etc. to Person

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,12 @@
 class Person
-  attr_reader :name
+  attr_reader :name, :age, :weight, :height, :eye_color, :hair_color
 
-  def initialize(name)
+  def initialize(name, options = {})
     @name = name
+    @age = options[:age] || 0
+    @weight = options[:weight] || rand(60..100)
+    @height = options[:height] || rand(150..200)
+    @eye_color = options[:eye_color]
+    @hair_color = options[:hair_color]
   end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -8,5 +8,25 @@ describe Person do
 
       expect(person.name).to eq 'Dave'
     end
+
+    it 'assigns default values to age, weight, height, eye_color, and hair_color' do
+      person = Person.new('Matt')
+
+      expect(person.age).to eq 0
+      expect(60..100).to include person.weight
+      expect(150..200).to include person.height
+      expect(person.eye_color).to be nil
+      expect(person.hair_color).to be nil
+    end
+
+    it 'assigns provided values for age, weight, height, eye_color, and hair_color' do
+      person = Person.new('Mike', age: 27, weight: 80, height: 170, eye_color: 'blue', hair_color: 'brown')
+
+      expect(person.age).to eq 27
+      expect(person.weight).to eq 80
+      expect(person.height).to eq 170
+      expect(person.eye_color).to eq 'blue'
+      expect(person.hair_color).to eq 'brown'
+    end
   end
 end
