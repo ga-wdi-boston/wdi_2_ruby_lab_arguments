8f360aa2516a26c811c2cf9855f0289d0c6f788e
Alex Grant <alex@grantovich.net>
2014-01-14 13:54:17 -0500

    Polygons have a number of corners

--- lib/polygon.rb
+++ lib/polygon.rb
@@ -4,4 +4,8 @@ class Polygon
   def initialize(*side_lengths)
     @side_lengths = side_lengths
   end
+
+  def number_of_corners
+    @side_lengths.length
+  end
 end
--- spec/polygon_spec.rb
+++ spec/polygon_spec.rb
@@ -2,13 +2,25 @@ require 'spec_helper'
 require_relative '../lib/polygon'
 
 describe Polygon do
+  before do
+    @rectangle = Polygon.new(2, 2, 4, 4)
+    @septagon = Polygon.new(1, 2, 3, 4, 5, 6, 7)
+    @triangle = Polygon.new(3, 4, 5)
+  end
+
   describe '#initialize' do
     it 'assigns side lengths' do
-      rectangle = Polygon.new(2, 2, 4, 4)
-      septagon = Polygon.new(1, 2, 3, 4, 5, 6, 7)
+      expect(@rectangle.side_lengths).to match_array [2, 2, 4, 4]
+      expect(@septagon.side_lengths).to match_array [1, 2, 3, 4, 5, 6, 7]
+      expect(@triangle.side_lengths).to match_array [3, 4, 5]
+    end
+  end
 
-      expect(rectangle.side_lengths).to match_array [2, 2, 4, 4]
-      expect(septagon.side_lengths).to match_array [1, 2, 3, 4, 5, 6, 7]
+  describe '#number_of_corners' do
+    it 'returns the number of corners in the polygon' do
+      expect(@rectangle.number_of_corners).to eq 4
+      expect(@septagon.number_of_corners).to eq 7
+      expect(@triangle.number_of_corners).to eq 3
     end
   end
 end
