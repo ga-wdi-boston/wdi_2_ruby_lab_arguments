7c16f35ecc277ea115cfd2318ea8ca6455a11361
tsyan <anna.tsykalova@gmail.com>
2014-01-14 01:15:47 -0500

    defined polygon class

--- spec/polygon_spec.rb
+++ spec/polygon_spec.rb
@@ -1,2 +1,35 @@
 require 'spec_helper'
 require_relative '../lib/polygon'
+
+describe Polygon do
+
+	describe '#initialize' do
+		it 'assigns the number of sides' do
+			polygon_1 = Polygon.new(9)
+			expect(polygon_1.number_of_sides).to eq 9
+		end
+	end
+
+	describe '#sum_of_interior_angles' do
+		it 'sums the interior angles' do
+			polygon_1 = Polygon.new(9)
+			expect(polygon_1.sum_of_interior_angles).to eq 7*180
+		end
+	end
+
+	describe '#number_of_corners' do
+		it 'calculates the number of corners' do
+			polygon_1 = Polygon.new(9)
+			expect(polygon_1.number_of_corners).to eq 9
+		end
+	end
+
+	describe '#degrees_of_freedom' do
+		it 'calculates the degrees of freedom' do
+			polygon_1 = Polygon.new(9)
+			expect(polygon_1.degrees_of_freedom).to eq 18
+		end
+	end
+
+
+end
