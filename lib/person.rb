a6a617ba1f94d20753fe1a7d1e002c16e545ed85
Praggya Rustagi Barretto <praggya@gmail.com>
2014-01-13 21:12:43 -0500

    changed person to use options hash

--- lib/person.rb
+++ lib/person.rb
@@ -1,19 +1,13 @@
 class Person
 	attr_accessor :name, :age, :weight, :height, :eye_color, :hair_color, :pets
 
-	def initialize (name, age=0, weight, height, eye_color, hair_color, pets)
+	def initialize (name, options={})
 		@name = name
-		@age = age
-		@weight = [60..100].sample
-		@height = [150..200].sample
-		@eye_color = eye_color
-		@hair_color = hair_color
-		@pets = [] << pets
+		@age = options[:age] || 0
+		@weight = options[:weight] || [60..100].sample
+		@height = options[:height] || [150..200].sample
+		@eye_color = options[:eye_color]
+		@hair_color = options[:hair_color]
+		@pets = [] << options[:pets]
 	end
 end
-
-def stones(weight)
-	weight * 0.157473
-end
-
-weight = 67
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -3,45 +3,45 @@ require_relative '../lib/person'
 
 describe Person do
 	it 'includes all options if all are specified' do
- 		person1 = Person.new("Bob", 34, 80, 175, "blue", "blonde", 6)
+ 		person1 = Person.new("Bob", :age => 34, :weight => 80, :height => 175, :eye_color => "blue", :hair_color => "blonde", :pets => 6)
 
  		expect(person1.name).to eq "Bob"
  		expect(person1.age).to eq 34
- 		expect([60..100]).to include person1.weight
- 		expect([150..200]).to include person1.height
+ 		expect(person1.weight).to eq 80
+ 		expect(person1.height).to eq 175
  		expect(person1.eye_color).to eq "blue"
  		expect(person1.hair_color).to eq "blonde"
  		expect(person1.pets).to eq [6]
  	end
 
  	it 'includes age of 0 if not specified' do
- 		person2 = Person.new("Cate", 74, 156, "brown", "brown", 3)
+ 		person1 = Person.new("Bob", :weight => 80, :height => 175, :eye_color => "blue", :hair_color => "blonde", :pets => 6)
 
- 		expect(person2.age).to eq 0
+ 		expect(person1.age).to eq 0
  	end
 
  	it 'includes random weight if not specified' do
- 		person3 = Person.new("Dirk", 25, 183, "green", "purple", 8)
+ 		person1 = Person.new("Bob", :age => 34, :height => 175, :eye_color => "blue", :hair_color => "blonde", :pets => 6)
 
- 		expect([60..100]).to include person3.weight
+ 		expect([60..100]).to include person1.weight
  	end
 
  	it 'includes random height if not specified' do
- 		person4 = Person.new("Barry", 73, 96, "hazel", "white", 1)
+ 		person1 = Person.new("Bob", :age => 34, :weight => 80, :eye_color => "blue", :hair_color => "blonde", :pets => 6)
 
- 		expect([150..200]).to include person4.height
+ 		expect([150..200]).to include person1.height
  	end
 
  	it 'includes no specified eye color' do
- 		person5 = Person.new("Carol", 15, 61, 152, "", "red", 2)
+ 		person1 = Person.new("Bob", :age => 34, :weight => 80, :height => 175, :hair_color => "blonde", :pets => 6)
 
- 		expect(person5.eye_color).to eq ''
+ 		expect(person1.eye_color).to eq nil
  	end
 
  	it 'includes no specified hair color' do
- 		person6 = Person.new("Joe", 35, 81, 192, "violet", "", 5)
+ 		person1 = Person.new("Bob", :age => 34, :weight => 80, :height => 175, :eye_color => "blue", :pets => 6)
 
- 		expect(person6.hair_color).to eq ""
+ 		expect(person1.hair_color).to eq nil
  	end
 
  	# it '#stones' do
@@ -49,10 +49,4 @@ describe Person do
 
  	# 	expect(person7.stones).to eq 10.550691
  	# end
-end
-
-describe 'stones' do
-  weight = 67
-
-  expect(stones).to eq 10.550691
 end
