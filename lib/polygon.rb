4b7321af4893dc074215edfc520eb7b2f91b8828
Jack Henley <jack.d.henley@gmail.com>
2014-01-14 08:58:35 -0500

    Corrected polygons: passing in length of sides instead of just number of sides

--- lib/polygon.rb
+++ lib/polygon.rb
@@ -1,23 +1,24 @@
-
+require 'pry'
 
 class Polygon
 
-	attr_accessor :sides
+	attr_accessor :sides, :num_sides
 
-	def initialize(sides)
-		@sides = sides
+	def initialize(options)
+		@sides = options[:sides]
+		@num_sides = @sides.count
 	end
 
 	def int_angles
-		180*(@sides-2)
+		180*(@num_sides-2)
 	end
 
 	def num_corners
-		@sides
+		@num_sides
 	end
 
 	def deg_freedom
-		@sides*2
+		@num_sides * 2
 	end
 
 	def name_poly
@@ -36,7 +37,7 @@ class Polygon
 			14 => 'tetrakaidecagon',
 			15 => 'pentadecagon'
 		}
-		names.fetch(@sides)
+		names.fetch(@num_sides)
 	end
 
 end
--- spec/polygon_spec.rb
+++ spec/polygon_spec.rb
@@ -5,49 +5,72 @@ require_relative '../lib/polygon'
 describe Polygon do
 
 	describe '#initialize' do
+		before(:each) do
+			@side1 = 2
+			@side2 = 3
+			@side3 = 4
+		end
 
 		it 'will return the number of sides when passed in' do
-			new_poly = Polygon.new(4)
-			expect(new_poly.sides).to eq 4
+			new_poly = Polygon.new(sides: [@side1, @side2, @side3])
+			expect(new_poly.num_sides).to eq 3
 		end
 	end
 
 
 	describe '#int_angles' do
+		before(:each) do
+			@side1 = 2
+			@side2 = 3
+			@side3 = 4
+		end
 
 		it 'will return the sum of interior angles' do
-			new_poly = Polygon.new(6)
-			expect(new_poly.int_angles).to eq 720
+			new_poly = Polygon.new(sides: [@side1, @side2, @side3])
+			expect(new_poly.int_angles).to eq 180
 
 		end
 	end
 
 	describe '#num_corners' do
+		before(:each) do
+			@side1 = 2
+			@side2 = 3
+			@side3 = 4
+		end
 
 		it 'will return the number of corners' do
-			new_poly = Polygon.new(6)
-			expect(new_poly.num_corners).to eq 6
+			new_poly = Polygon.new(sides: [@side1, @side2, @side3])
+			expect(new_poly.num_corners).to eq 3
 
 		end
 	end
 
 	describe '#deg_freedom' do
+		before(:each) do
+			@side1 = 2
+			@side2 = 3
+			@side3 = 4
+		end
 
-		it 'will return the number of corners' do
-			new_poly = Polygon.new(6)
-			expect(new_poly.deg_freedom).to eq 12
+		it 'will return the degrees of freedom' do
+			new_poly = Polygon.new(sides: [@side1, @side2, @side3])
+			expect(new_poly.deg_freedom).to eq 6
 
 		end
 	end
 
 	describe '#name_poly' do
+		before(:each) do
+			@side1 = 2
+			@side2 = 3
+			@side3 = 4
+		end
 
 		it 'will return the correct name based on the number of sides' do
-			new_poly = Polygon.new(5)
-			new_poly2 = Polygon.new(6)
+			new_poly = Polygon.new(sides: [@side1, @side2, @side3])
 
-			expect(new_poly.name_poly).to eq "pentagon"
-			expect(new_poly2.name_poly).to eq "hexagon"
+			expect(new_poly.name_poly).to eq "triangle"
 
 		end
 
